8f817420ea2c71122c75dee5ab6a34aa70c9f12f
kim tea yeon <122727774+kimty9627@users.noreply.github.com>
2023-04-11 21:13:29 +0900

    Init:login,join,logout update

--- commit/models.py
+++ commit/models.py
@@ -1,6 +1,6 @@
 # tweet/models.py
 from django.db import models
-from user.models import models
+from user.models import UserModel
 
 
 # Create your models here.
@@ -8,7 +8,7 @@ class Commit(models.Model):
     class Meta:
         db_table = "commit"
 
-    author = models.ForeignKey(models, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     content = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- user/models.py
+++ user/models.py
@@ -1,9 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, nickname,password=None):
+        if not nickname:
+            raise ValueError("Users must have an email address")
+        
+        user = self.model(
+            nickname=nickname,
+            
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname,password=None):
+        user = self.create_user(
+            nickname=nickname,
+            password=password,
+            
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+        
+        
 class UserModel(AbstractBaseUser):
+    nickname = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=10)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
-        db_table = "my_user"
+        db_table = 'user'
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'nickname'
+    REQUIRED_FIELDS = ['name']
